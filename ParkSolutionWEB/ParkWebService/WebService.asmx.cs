91fe538050a1a0962d0aa414eeb787aaafcbcbf4
Carlos Sales <causalvador@hotmail.com>
2017-03-22 01:16:34 -0300

    Registro Entrada e Saida - OK

--- ParkSolutionWEB/ParkWebService/WebService.asmx.cs
+++ ParkSolutionWEB/ParkWebService/WebService.asmx.cs
@@ -29,9 +29,9 @@ namespace ParkWebService
             string retorno = "REGISTRO DE ENTRADA - TENTATIVA";
             OperacaoBanco operacao2 = new OperacaoBanco();
 
-            string strInsert = "insert into Tbl_Movimento  (ID_Controle , ID_Operador, Placa, Data_Entrada, Setor_Entrada, StatusID ) " +
-                               "values (" + param1 + ", " + param2 + ", '" + param3 + "', '" + param4 + "','" + param5 + "','" + param6 + "')";
-            Boolean inserir2 = operacao2.Insert(strInsert );
+            string strInsert = "insert into Tbl_Movimento  (ID_Controle , ID_Operador, Placa, Data_Entrada, Setor_Entrada, encerrado ) " +
+                               "values (" + param1 + ", " + param2 + ", '" + param3 + "', '" + param4 + "', '" + param5 + "',0)";
+            Boolean inserir2 = operacao2.Insert(strInsert);
             ConexaoBancoSQL.fecharConexao();
 
             if (inserir2 == true)
@@ -41,5 +41,32 @@ namespace ParkWebService
 
             return retorno;
         }
+
+        [WebMethod]
+        public string RegistraSaida(string param1, string param2, string param3, string param4, string param5, string param6, string param7, string param8)
+        {
+            string retorno = "REGISTRO DE SAIDA - TENTATIVA";
+            OperacaoBanco operacao3 = new OperacaoBanco();
+
+            string strUpdate = "update Tbl_Movimento set " +
+                               "ID_Operador = " + param2 + "," +
+                               "Data_Saida ='" + param3 + "'," +
+                               "Permancencia  =" + param4 + "," +
+                               "Ocorrencia ='" + param5 + "'," +
+                               "Setor_Saida ='" + param6 + "'," +
+                               "ID_Lojista =" + param7 + "," +
+                               "Obs  ='" + param8 + "'," +
+                               "encerrado = 1 " +
+                               "where ID_Controle = " + param1;
+
+            Boolean update = operacao3.Update(strUpdate);
+            if (update == true)
+            {
+                retorno = "OK. GLORIFICADO SEJA O NOME DO SENHOR JESUS CRISTO";
+            }
+            ConexaoBancoSQL.fecharConexao();
+            return retorno;
+        }
+
     }
 }
